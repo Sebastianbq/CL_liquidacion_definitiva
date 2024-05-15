6a5c989d11ec67d79d355f8a1ea8f61bd62eb302
Sebastianbq <141242964+Sebastianbq@users.noreply.github.com>
2024-05-15 08:30:30 -0500

    Update SecretConfig.py

--- SecretConfig.py
+++ SecretConfig.py
@@ -1,5 +1,5 @@
-PGHOST='ep-fragrant-river-a5e4yp1r.us-east-2.aws.neon.tech'
-PGDATABASE='worker'
-PGUSER='worker_owner'
-PGPASSWORD='nCfmLE09WVsi'
-PGPORT= 5432
+PGHOST=
+PGDATABASE=
+PGUSER=
+PGPASSWORD=
+PGPORT=
